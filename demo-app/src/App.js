1615049ebf9e5dbac5dd00751fade8e424c4c6c3
Vismaya Wilson <vismayawilson3@gmail.com>
2022-01-23 14:06:22 +0530

    React-routing

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link, Switch, Route, Redirect } from "react-router-dom";
+import Susmitha from "./components/Home";
+import Sherly from "./components/About";
 
 function App() {
+  function Users() {
+    return <h2>Users</h2>;
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+        {/* <li><Link to="/us">US</Link></li> */}
+      </ul>
+      <Switch>
+        <Route exact path="/">
+          <Susmitha />
+        </Route>
+        <Route path="/us">
+          <Redirect to="/about" />
+        
+        </Route>
+        <Route path="/xyz">
+          <Redirect to="/about" />
+        
+        </Route>
+        <Route path="/about">
+          <Sherly />
+        </Route>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/**">
+          <h2>Wrong end point</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
